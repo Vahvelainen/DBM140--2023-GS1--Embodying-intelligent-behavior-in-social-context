b9fe0835fee4a5f41123d6844528fd4999bc0e48
vahvake <62074587+Vahvelainen@users.noreply.github.com>
2023-10-10 18:15:16 +0200

    Add colors to console

--- RobotTamagotchi/main.py
+++ RobotTamagotchi/main.py
@@ -12,12 +12,14 @@ initial_policy = sarsa.Q.copy()
 
 reward = 0
 for step_index in range(steps_N):
-  print('\nSTEP ' + str(step_index))
+  #Print the step in green
+  print('\033[92m' + '\nSTEP ' + str(step_index) + '\033[0m')
 
   #Set Task Undone avery 10th step
   if ( step_index%10 == 0):
     task = sarsa.state.getVar('Task')
-    print('It is time for a new task. Previous task: ' + task.value )
+    #Print the new task and previous task status in yellow
+    print('\033[93m' + 'It is time for a new task. Previous task: ' + task.value  + '\033[0m')
     task.set('Undone')
 
   #calculate changes to Q table for last action taken and return new action
